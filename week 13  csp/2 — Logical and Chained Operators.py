4a44f4dafe8b3c538f2883659accd32a30962a66
yeezy911 <rmoralez4901@gmail.com>
2025-11-13 19:09:32 +0000

    grade checker

--- week 13  csp/2 — Logical and Chained Operators.py
+++ week 13  csp/2 — Logical and Chained Operators.py
@@ -12,14 +12,35 @@
 # You can chain comparisons: 1 < x < 5
 
 # Examples:
-x = 10
-print(x > 5 and x < 15)   # True
-print(x < 5 or x == 10)   # True
-print(not(x == 10))       # False
-print(1 < x < 20)         # True
+# x = 10
+# print(x > 5 and x < 15)   # True
+# print(x < 5 or x == 10)   # True
+# print(not(x == 10))       # False
+# print(1 < x < 20)         # True
 
 
 # Practice Problems:
+#scpre / grade checker
+
+grade = int(input("What is your score?"))
+# if the students grade is between 90 and 100 inclusive, print "A"
+if grade >= 90 and grade <= 100:
+    print("You got a A")
+# if the students grade is between 80 and 90 inclusive, print "B"
+elif grade >= 80 and grade < 90:
+    print("You got a B")
+# if the students grade is between 70 and 80 inclusive, print "C"
+elif grade >= 70 and grade < 80:
+    print("You got a C")
+# if the students grade is between 60 and 70 inclusive, print "D"
+elif grade >= 60 and grade < 70:
+    print("You got a D")
+# if the students grade is below 70, print "F"
+else:
+    print("You got a F")
+
+
+
 
 # Write an expression that checks if a number is between 50 and 100 (inclusive).
 
